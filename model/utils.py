befb0639b881b351d1c9370670a3229da7665281
yuhuili <81251885+Liyuhui-12@users.noreply.github.com>
2024-02-03 19:45:56 +0800

    Update utils.py

--- model/utils.py
+++ model/utils.py
@@ -19,18 +19,22 @@ from transformers.generation.logits_process import (
 
 
 def prepare_logits_processor(
-        temperature=0.0, repetition_penalty=0.0, top_p=0.0, top_k=0
+        temperature: float = 0.0, 
+        repetition_penalty: float = 0.0, 
+        top_p: float = 0.0, 
+        top_k: int = 0
 ) -> LogitsProcessorList:
     processor_list = LogitsProcessorList()
-    if temperature >= 1e-5 and temperature != 1.0:
-        processor_list.append(TemperatureLogitsWarper(temperature))
-    if repetition_penalty > 1.0:
-        processor_list.append(RepetitionPenaltyLogitsProcessor(repetition_penalty))
-    if 1e-8 <= top_p < 1.0:
-        processor_list.append(TopPLogitsWarper(top_p))
-    if top_k > 0:
-        processor_list.append(TopKLogitsWarper(top_k))
-    return processor_list
+    if temperature>1e-5:
+        if temperature >= 1e-5 and temperature != 1.0:
+            processor_list.append(TemperatureLogitsWarper(temperature))
+        if repetition_penalty > 1.0:
+            processor_list.append(RepetitionPenaltyLogitsProcessor(repetition_penalty))
+        if 1e-8 <= top_p < 1.0:
+            processor_list.append(TopPLogitsWarper(top_p))
+        if top_k > 0:
+            processor_list.append(TopKLogitsWarper(top_k))
+        return processor_list
 
 
 # test_processor = prepare_logits_processor(
