39a0b86eb33b4cb78784a6f627a058666491546b
yuhuili <81251885+Liyuhui-12@users.noreply.github.com>
2024-05-26 23:51:52 +0800

    Update gen_baseline_answer_vicuna.py

--- eagle/evaluation/gen_baseline_answer_vicuna.py
+++ eagle/evaluation/gen_baseline_answer_vicuna.py
@@ -24,7 +24,7 @@ from ..model.choices import *
 
 
 
-def ea_forward(input_ids, model, tokenizer, tree_choices, logits_processor=None, max_steps=512):
+def ea_forward(input_ids, model, tokenizer, tree_choices, logits_processor=None, max_steps=2048):
     assert input_ids.shape[0] == 1, "Only support batch size 1 for now!!"
     # Avoid modifying the input_ids in-place
     input_ids = input_ids.clone()
