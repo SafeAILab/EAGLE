32f612cbd1314ea4aa02f0e0c0ab47f74cddc12f
yunhai <yh5961@nyu.edu>
2025-09-10 13:52:41 +0000

    update

--- eagle/evaluation/gen_ea_answer_qwen3.py
+++ eagle/evaluation/gen_ea_answer_qwen3.py
@@ -212,7 +212,7 @@ def get_model_answers(
 
                 torch.cuda.synchronize()
                 start_time = time.time()
-                output_ids, new_token, idx, metrics = model.eagenerate_with_metrics(
+                output_ids, new_token, idx = model.eagenerate(
                     torch.as_tensor(input_ids).cuda(),
                     temperature=temperature,
                     log=True
@@ -254,7 +254,7 @@ def get_model_answers(
                 wall_time.append(total_time)
                 conv.messages[-1][-1] = output
             # torch.cuda.empty_cache()
-            choices.append({"index": i, "turns": turns, "idxs": idxs, "new_tokens": new_tokens, "wall_time": wall_time, "metrics": metrics})
+            choices.append({"index": i, "turns": turns, "idxs": idxs, "new_tokens": new_tokens, "wall_time": wall_time})
 
         # Dump answers
         os.makedirs(os.path.dirname(answer_file), exist_ok=True)
